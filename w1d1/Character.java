9aa8fa1548b947b8d467add7f0a8599a8949b349
Saravananmuthaiya <pmsaravanan28@gmail.com>
2023-08-20 12:21:13 +0530

    Add files via upload
    Week1Day1

---
+++ w1d1/Character.java
@@ -0,0 +1,28 @@
+package w1d1;
+
+public class Character {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String Name="Saravanan";
+		int  age = 25;
+		long  creditCardNum    = 18391831938l;
+		float  mileage         = 20.5f;
+		double  bankBalance = 6549.82983;
+		boolean  hadLunch = true;
+		char  logo     = 'H';
+		String colour="RED";
+		String  brandName  =  "Audi";
+		
+		System.out.println("Name : "+Name);
+		System.out.println("My age is : "+age);
+		System.out.println("The credit card number is : "+creditCardNum +" The mileage is : " +mileage);
+		System.out.println("The logo of my car is : "+logo);
+		System.out.println("Bank balance is "+bankBalance);
+		System.out.println("The brand name is :"+brandName);
+		System.out.println(hadLunch);
+		System.out.println("Vehicle color: "+ colour);
+		
+	}
+
+}
---
+++ w1d1/Number.java
@@ -0,0 +1,21 @@
+package w1d1;
+
+public class Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int a=5,b=9,c;
+    c=a+b;
+    System.out.println("The output value is: "+c);
+    
+    if (c/2==0) {
+    	System.out.println("given number is even: "+c);
+		
+	} else {
+		System.out.println("Given number is odd: "+c);
+	}
+	}
+	
+	
+
+}
